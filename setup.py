f0f10a4460c75c6c66f0988f8abca4ff29d603bb
samdixon <sdixon@posteo.net>
2020-06-15 11:52:38 -0400

    updates build deps/versions

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/samdixon/easy-publish"
 EMAIL = "sdixon@posteo.net"
 AUTHOR = "Samuel Dixon"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.0.1"
+VERSION = "0.0.4"
 
 # What packages are required for this module to be executed?
 REQUIRED = []
