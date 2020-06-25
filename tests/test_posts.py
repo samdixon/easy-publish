16d51324f41c5e045cd4374d8857510ea55c1253
samdixon <sdixon@posteo.net>
2020-06-25 15:09:05 -0400

    black linting, updates tests
    Black linter added.
    
    Adds fixtures for a couple items that allow reuse. Not sure if fixtures
    are the correct way to go about testing but they seem to be working okay
    for now. Changes import scheme which allowed me to fix the issue I was
    having with __all__ in posts.py.

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,39 +1,59 @@
 import pytest
-from easy_publish import generate_posts, generate_parsed_post_list, PostParser, PostCollection
+from easy_publish import posts
 
 
 @pytest.fixture(scope="module")
 def fakenotes_files():
-    return ['./tests/fakenotes/note.md',
-             './tests/fakenotes/other.md',
-             './tests/fakenotes/other-note-two.md']
+    return [
+        "./tests/fakenotes/note.md",
+        "./tests/fakenotes/other.md",
+        "./tests/fakenotes/other-note-two.md",
+    ]
+
 
 @pytest.fixture(scope="module")
 def fakenotes_file():
-    return './tests/fakenotes/note.md'
+    return "./tests/fakenotes/note.md"
+
 
 @pytest.fixture(scope="module")
 def parsed_posts(fakenotes_files):
-    parsed_posts = generate_parsed_post_list(fakenotes_files)
+    parsed_posts = posts.generate_parsed_post_list(fakenotes_files, "%m-%d-%y")
     return parsed_posts
+
+
+@pytest.fixture(scope="module")
+def PostsCollection(parsed_posts):
+    posts_collection = posts.PostsCollection(parsed_posts)
+    return posts_collection
+
+
+@pytest.fixture(scope="module")
+def MetadataCollection(parsed_posts):
+    metadata_collection = posts.MetadataCollection(parsed_posts, "ascending")
+    return metadata_collection
+
 
 def test_generate_posts():
     # incomplete
-    posts = generate_posts("./tests/fakenotes")
-    assert posts.__class__.__name__ == 'PostCollection'
+    p = posts.generate_posts("./tests/fakenotes")
+    assert p.__class__.__name__ == "Posts"
 
-def test_PostCollection(parsed_posts):
+
+def test_Posts(PostsCollection, MetadataCollection):
     # incomplete
-    post_collection = PostCollection(parsed_posts)
-    assert post_collection.posts.__class__.__name__ == "dict"
-    assert post_collection.metadata.__class__.__name__ == "list"
+    p = posts.Posts(PostsCollection.posts, MetadataCollection.metadata)
+    assert PostsCollection.posts.__class__.__name__ == "dict"
+    assert MetadataCollection.metadata.__class__.__name__ == "list"
+
 
 def test_Metadata():
     pass
 
+
 def test_PostParser(fakenotes_file):
     # incomplete
-    p = PostParser(fakenotes_file)
+    p = posts.PostParser(fakenotes_file, "date")
     assert p.file == "./tests/fakenotes/note.md"
     assert p.title == "This is the fakenote"
     assert p.date == "5-23-94"
